6c788e85c93fa916dd1cfeb2dcef935a8ba00b56
howell47 <44988974+howell47@users.noreply.github.com>
2018-11-12 21:07:21 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN">
+<html lang="en">
+<head>
+<meta name="viewport" content=
+"width=device-width, initial-scale=1">
+<meta name="generator" content=
+"HTML Tidy for Linux (vers 14 June 2007), see www.w3.org">
+<meta http-equiv="Content-Type" content=
+"text/html; charset=us-ascii">
+<meta content="MSHTML 6.00.2800.1400" name="GENERATOR">
+<title>WhereHouse</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap-theme.min.css">
+</head>
+
+<body>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+<div id="main">
+   <div class="container">
+	   <div id="header">
+		   <div id="logo">
+    		   <h1>Logo</h1>
+		   </div>
+
+    		<div id="tagline">
+        	   <h3>WhereHouse</h3>
+		   </div>
+         <div style="clear:both"></div>
+    		
+         <ul id="menu">
+           <li><a href="#">Home</a></li> <!index.php> <!page where the customer login will be>
+           <li><a href="#">About Us</a></li> <!aboutus.php>
+           <li><a href="#">Contact Us</a></li> <!contactus.php>
+           <li><a href="#">Contact</a></li>
+	       </ul>
+         <div style="clear:both"></div>
+      </div>
+
+      <div id="content">
+        <h2>This will be the Homepage/Login Page</h2>
+        <h3>The Login will be here </h3>
+        <p>Fake Paragraph  </p>
+        <p>Another Fake Paragraph </p>
+   		</div>
+     
+		<div id="sidebar">
+                <div id="popular">
+                    <h3>We probably won't need this</h3>
+                    <ul>
+                        <li><a href="#">Some fake link</a></li>
+                    </ul>
+                </div>
+
+                <div id="cool">
+                    <h3>More filler content</h3>
+                    <ul>
+                        <li><a href="#">Some fake link</a></li>
+                    </ul>
+               </div>
+      </div>
+      <div style="clear:both"></div>
+
+   </div>
+         
+   <div id="footer">
+        <div class="container">
+            <p>Our footer will have other ways to interact w/ us i.e. Facebook, Twitter, Facebook</p>
+        </div>
+    </div>
+</div>
+</body>
+</html>
